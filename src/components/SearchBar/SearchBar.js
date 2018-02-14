286f7f4c2634f0ee71dd13e93f818131abd4c7f0
Robert Wolff <ro.wolff@gmx.net>
2018-02-14 16:44:06 +0100

    Bind search and location inputs to event handlers

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,6 +15,9 @@ class SearchBar extends React.Component {
       location: '',
       sortBy: 'best_match'
     }
+
+    this.handleTermChange = this.handleTermChange.bind(this)
+    this.handleLocationChange = this.handleLocationChange.bind(this)
   }
 
 
@@ -28,6 +31,14 @@ class SearchBar extends React.Component {
 
   handleSortByChange(sortByOption) {
     this.setState({sortBy: sortByOption})
+  }
+
+  handleTermChange(event) {
+    this.setState({term: event.target.value})
+  }
+
+  handleLocationChange(event) {
+    this.setState({location: event.target.value})
   }
 
   renderSortByOptions() {
@@ -55,8 +66,14 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input
+            placeholder="Search Businesses"
+            onChange={this.handleTermChange}
+          />
+          <input
+            placeholder="Where?"
+            onChange={this.handleLocationChange}
+          />
           </div>
         <div className="SearchBar-submit">
           <a>{"Let's Go"}</a>
